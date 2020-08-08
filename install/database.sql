0869623c9353f50858399215a9c98d26af2f5b95
Miroslav Paulík <castamir@users.noreply.github.com>
2020-08-08 07:06:34 +0200

    Update database.sql

--- install/database.sql
+++ install/database.sql
@@ -1,9 +1,10 @@
-CREATE TABLE  "AGENTS" 
-(	
-  "AGENT_CODE" CHAR(6) NOT NULL PRIMARY KEY, 
-	"AGENT_NAME" CHAR(40), 
-	"WORKING_AREA" CHAR(35), 
-	"COMMISSION" NUMBER(10,2), 
-	"PHONE_NO" CHAR(15), 
-	"COUNTRY" VARCHAR2(25) 
-);
+DROP TABLE IF EXISTS "public"."agents";
+CREATE TABLE "public"."agents" (
+  "id" uuid NOT NULL DEFAULT shared_extensions.gen_random_uuid(),
+  "agent_name" varchar(40) COLLATE "pg_catalog"."default" NOT NULL,
+  "working_area" varchar(35) COLLATE "pg_catalog"."default" NOT NULL,
+  "commission" numeric(25,6),
+  "phone_number" varchar(15) COLLATE "pg_catalog"."default" NOT NULL,
+  "country" varchar(25) COLLATE "pg_catalog"."default" NOT NULL
+)
+;
